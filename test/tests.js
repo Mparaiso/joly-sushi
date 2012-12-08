461d75ab6680314e3ec991c68e7ae9d41565414b
mparaiso <aikah@free.fr>
2012-12-08 06:15:22 +0100

    step6

--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,7 @@ module("ProductListController",{
         //@note @angular effectuer des tests unitaires avec angular
         this.scope= {};
         this.productListController = new ProductListController(this.scope);
+        // browser().navigateTo("../index.htm");
     }
 });
 
@@ -10,3 +11,4 @@ test("ProductListController",function(){
     equal(this.scope.sushis.length,6,"sushis list has 6 elements");
     equal(this.scope.sushis[0].type,"sushi","the first element is a sushi");
 });
+
